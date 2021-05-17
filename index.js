c1455e31e3649d3e9ae624f4d5b654c57f878bb1
Arthur V Fain <arthurvfain@gmail.com>
2021-05-17 18:16:32 -0400

    commit tree 76970a41263f55128cd2efd1c6aa99db964dfbdd

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 19;
+const num4 = 5;
+let set = [1,2,4,5,10,20];
+let multiply =  num1 * num2;
+let random = Math.ceil(Math.random() * 10);
+let mod = num3 % num4;
+let max = Math.max(...set);
